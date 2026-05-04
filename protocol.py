b3db5b464ff39590cac25af45fbe8b2dcf25ddc7
iroha <78338683+little-jan@users.noreply.github.com>
2026-05-04 18:50:39 +0800

    basic classes in protocol.py but nothing implemented

---
+++ protocol.py
@@ -0,0 +1,43 @@
+"""
+This file contains:
+
+Data link layer ->
+- frame creation
+- MAC addressing
+- forwarding behaviour
+- delivery of data to upper layer
+
+Network layer ->
+- packet creation
+- routing decisions
+- forwarding between networks
+- basic control mechanisms
+
+Transport layer ->
+- data segmentation
+- construction of transport segments
+- error detection
+- simple reliable data transfer mechanism
+"""
+
+
+# classes for IP and MAC addresses
+class IP:
+    def __init__(self):
+
+
+class MAC:
+    def __init__(self):
+
+
+# classes for simplified network stack layers
+class DataLink:
+    def __init__(self):
+
+
+class Network:
+    def __init__(self):
+
+
+class Transport:
+    def __init__(self):
